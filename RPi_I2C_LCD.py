2a661387dd26b5aab0b7a0c2a1da9a577bc0cb43
mitag <micha.aus.berlin@gmail.com>
2017-12-07 20:06:52 +0100

    add optional parameter for i2c-bus to class LCD

--- RPi_I2C_LCD.py
+++ RPi_I2C_LCD.py
@@ -18,53 +18,54 @@ import smbus
 from time import *
 
 # some const
-# LCD Address
+# LCD Address and I2C Bus number
+I2C_PORT = 1
 LCD_ADDRESS = 0x27
 
 # commands
-LCD_CLEARDISPLAY		= 0x01
-LCD_RETURNHOME			= 0x02
-LCD_ENTRYMODESET		= 0x04
-LCD_DISPLAYCONTROL		= 0x08
-LCD_CURSORSHIFT			= 0x10
-LCD_FUNCTIONSET			= 0x20
-LCD_SETCGRAMADDR		= 0x40
-LCD_SETDDRAMADDR		= 0x80
+LCD_CLEARDISPLAY = 0x01
+LCD_RETURNHOME = 0x02
+LCD_ENTRYMODESET = 0x04
+LCD_DISPLAYCONTROL = 0x08
+LCD_CURSORSHIFT = 0x10
+LCD_FUNCTIONSET = 0x20
+LCD_SETCGRAMADDR = 0x40
+LCD_SETDDRAMADDR = 0x80
 
 # flags for display entry mode
-LCD_ENTRYRIGHT			= 0x00
-LCD_ENTRYLEFT			= 0x02
-LCD_ENTRYSHIFTINCREMENT	= 0x01
-LCD_ENTRYSHIFTDECREMENT	= 0x00
+LCD_ENTRYRIGHT = 0x00
+LCD_ENTRYLEFT = 0x02
+LCD_ENTRYSHIFTINCREMENT = 0x01
+LCD_ENTRYSHIFTDECREMENT = 0x00
 
 # flags for display on/off control
-LCD_DISPLAYON			= 0x04
-LCD_DISPLAYOFF			= 0x00
-LCD_CURSORON			= 0x02
-LCD_CURSOROFF			= 0x00
-LCD_BLINKON				= 0x01
-LCD_BLINKOFF			= 0x00
+LCD_DISPLAYON = 0x04
+LCD_DISPLAYOFF = 0x00
+LCD_CURSORON = 0x02
+LCD_CURSOROFF = 0x00
+LCD_BLINKON = 0x01
+LCD_BLINKOFF = 0x00
 
 # flags for display/cursor shift
-LCD_DISPLAYMOVE			= 0x08
-LCD_CURSORMOVE			= 0x00
-LCD_MOVERIGHT			= 0x04
-LCD_MOVELEFT			= 0x00
+LCD_DISPLAYMOVE = 0x08
+LCD_CURSORMOVE = 0x00
+LCD_MOVERIGHT = 0x04
+LCD_MOVELEFT = 0x00
 
 # flags for function set
-LCD_8BITMODE			= 0x10
-LCD_4BITMODE			= 0x00
-LCD_2LINE				= 0x08
-LCD_1LINE				= 0x00
-LCD_5x10DOTS			= 0x04
-LCD_5x8DOTS				= 0x00
+LCD_8BITMODE = 0x10
+LCD_4BITMODE = 0x00
+LCD_2LINE = 0x08
+LCD_1LINE = 0x00
+LCD_5x10DOTS = 0x04
+LCD_5x8DOTS = 0x00
 
 # offset for up to 4 rows
-LCD_ROW_OFFSET			= (0x80, 0xC0, 0x94, 0xD4)
+LCD_ROW_OFFSET = (0x80, 0xC0, 0x94, 0xD4)
 
 # flags for backlight control
-LCD_BACKLIGHT			= 0x08
-LCD_NOBACKLIGHT			= 0x00
+LCD_BACKLIGHT = 0x08
+LCD_NOBACKLIGHT = 0x00
 
 En = 0b00000100  # Enable bit
 Rw = 0b00000010  # Read/Write bit
@@ -72,7 +73,7 @@ Rs = 0b00000001  # Register select bit
 
 
 class I2cDevice:
-    def __init__(self, addr, port=1):
+    def __init__(self, addr, port):
         self.addr = addr
         self.bus = smbus.SMBus(port)
 
@@ -105,7 +106,7 @@ class I2cDevice:
 
 
 class LCD:
-    def __init__(self, i2c_addr=LCD_ADDRESS):
+    def __init__(self, i2c_addr=LCD_ADDRESS, port=I2C_PORT):
         """
         Init LCD class and panel
 
@@ -114,7 +115,7 @@ class LCD:
         """
         # private vars
         self._back_light = False
-        self._lcd_device = I2cDevice(i2c_addr)
+        self._lcd_device = I2cDevice(i2c_addr, port)
         # init display
         self.write_cmd(0x03)
         self.write_cmd(0x03)
